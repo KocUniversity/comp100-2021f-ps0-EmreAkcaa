1c488f2319ac900572697ac47399b9b26b744708
emreakca <91703295+EmreAkcaa@users.noreply.github.com>
2021-10-03 19:31:42 +0000

    This program executes 2 basic mathematical functions with given variables.

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy
+print("Hello World! :D")
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number(choose a relatively small number please): "))
+print("Exponential Product: ",x**y)
+print("Log2 of the first number: ",numpy.log2(x))
+print("My Student ID: 80567")
+print("--- Mission Completed ---")
